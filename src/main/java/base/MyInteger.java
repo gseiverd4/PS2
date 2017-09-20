7e9f9f4f22666c9835f11600bef8b11305fbd2d2
gregoryseiverd <gregoryseiverd@10.0.0.67>
2017-09-20 03:19:19 -0400

    PS2 Changes

--- src/main/java/base/MyInteger.java
+++ src/main/java/base/MyInteger.java
@@ -1,36 +1,29 @@
 package base;
 
-public class MyInteger {
+public class MyInteger 
+{
 	
 	private int Number;
 	
-	public MyInteger(int iValue) {
+	public MyInteger(int iValue) 
+	{
 		
 		Number = iValue;
 	}
 
-	public int getNumber() {
+	public int getNumber() 
+	{
 		
 		return Number;
 	}
 	
-	public boolean isEven() {
-		if (Number % 2 == 0)
-			return true;
-		else
-			return false;
-	}
-	
-	public boolean isOdd() {
-		if (Number % 2 != 0)
-			return true;
-		else
-			return false;
-	}
-	
-	public boolean isPrime() {
+	public boolean isPrime() 
+	{
 		int value = 0;
-		for (value = 2; value < Number; value++) {
+		for (value = 2; 
+				value < Number; 
+				value++) 
+		{
 			if (Number % value == 0)
 				return false;
 		}
@@ -38,46 +31,71 @@ public class MyInteger {
 	
 	}
 	
-	public static boolean isEven(int Number) {
+	public boolean isEven() 
+	{
 		if (Number % 2 == 0)
 			return true;
 		else
 			return false;
 	}
 	
-	public static boolean isOdd(int Number) {
+	public boolean isOdd() 
+	{
 		if (Number % 2 != 0)
 			return true;
 		else
 			return false;
 	}
 	
-	public static boolean isPrime(int Number) {
+	public static boolean isEven(int Number) 
+	{
+		if (Number % 2 == 0)
+			return true;
+		else
+			return false;
+	}
+	
+	public static boolean isOdd(int Number) 
+	{
+		if (Number % 2 != 0)
+			return true;
+		else
+			return false;
+	}
+	
+	public static boolean isPrime(int Number) 
+	{
 		int value = 0;
-		for (value = 2; value < Number; value++) {
+		for (value = 2; value < Number; value++) 
+		{
 			if (Number % value == 0)
 				return false;
 		}
 		return true;
 	}
 	
-	public static boolean isEven(MyInteger iValue) {
+	public static boolean isEven(MyInteger iValue) 
+	{
 		return MyInteger.isEven(iValue.getNumber());
 	}
 	
-	public static boolean isOdd(MyInteger iValue) {
+	public static boolean isOdd(MyInteger iValue) 
+	{
 		return MyInteger.isOdd(iValue.getNumber());
 	}
 	
-	public static boolean isPrime(MyInteger iValue) {
+	public static boolean isPrime(MyInteger iValue) 
+	{
 		return MyInteger.isPrime(iValue.getNumber());
 	}
 	
-	public boolean equals(int iValue) {
+	public boolean equals(int iValue) 
+	{
 		return getNumber() == iValue;
 	}
 	
-	public boolean equals(MyInteger iValue) {
+	public boolean equals(MyInteger iValue) 
+	{
 		return equals(iValue.getNumber());
 	}
 	
